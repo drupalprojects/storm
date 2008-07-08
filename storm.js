36da3881e7dd6f86c1346da21bbe1747626790bb
Roberto Gerola <roberto_gerola@46851.no-reply.drupal.org>
2008-07-08 06:56:39 +0000

    Backport to D5

--- storm.js
+++ storm.js
@@ -41,10 +41,9 @@ function storm_popup(sender, name, title, width, height, content_id, position) {
   var p_name = "storm" + name + '_popup';
   var p_close = "storm" + name + "_popup_close";
 
-  var elemOffset = storm_offset($(sender));
-  var top = elemOffset.top;
-  var left = elemOffset.left;
-alert(left);
+  var elemcoords = storm_getcoords(sender);
+  var top = elemcoords.y;
+  var left = elemcoords.x;
   switch (position) {
     case 'l':
       left = left-width;
@@ -82,10 +81,10 @@ alert(left);
   $("body").append(popup);
   var p = $("#" + p_name);
   p.css('position', 'absolute');
-  p.css('top', top);
-  p.css('left', left);
-  p.css('width', width);
-  p.css('height', height);
+  p.css('top', top + 'px');
+  p.css('left', left + 'px');
+  p.css('width', width + 'px');
+  p.css('height', height + 'px');
   p.show();
   $("#" + p_close).click(function(){
     $("#" + p_name).remove();
@@ -101,97 +100,13 @@ function storm_datext_tonull(sender, date_id) {
   }
 };
 
-function storm_offset(elem) {
-	var left = 0, top = 0, results;
-	if ( elem ) with ( jQuery.browser ) {
-		var parent       = elem.parentNode, 
-		    offsetChild  = elem,
-		    offsetParent = elem.offsetParent, 
-		    doc          = elem.get(0).ownerDocument,
-		    safari2      = safari && parseInt(version) < 522 && !/adobeair/i.test(userAgent),
-		    fixed        = elem.css("position") == "fixed";
-		// Use getBoundingClientRect if available
-
-		if ( elem.getBoundingClientRect ) {
-			var box = elem.getBoundingClientRect();
-		
-			// Add the document scroll offsets
-			add(box.left + Math.max(doc.documentElement.scrollLeft, doc.body.scrollLeft),
-				box.top  + Math.max(doc.documentElement.scrollTop,  doc.body.scrollTop));
-		
-			// IE adds the HTML element's border, by default it is medium which is 2px
-			// IE 6 and 7 quirks mode the border width is overwritable by the following css html { border: 0; }
-			// IE 7 standards mode, the border is always 2px
-			// This border/offset is typically represented by the clientLeft and clientTop properties
-			// However, in IE6 and 7 quirks mode the clientLeft and clientTop properties are not updated when overwriting it via CSS
-			// Therefore this method will be off by 2px in IE while in quirksmode
-			add( -doc.documentElement.clientLeft, -doc.documentElement.clientTop );
-	
-		// Otherwise loop through the offsetParents and parentNodes
-		} else {
-		
-			// Initial element offsets
-			add( elem.offsetLeft, elem.offsetTop );
-			
-			// Get parent offsets
-			while ( offsetParent ) {
-				// Add offsetParent offsets
-				add( offsetParent.offsetLeft, offsetParent.offsetTop );
-			
-				// Mozilla and Safari > 2 does not include the border on offset parents
-				// However Mozilla adds the border for table or table cells
-				if ( mozilla && !/^t(able|d|h)$/i.test(offsetParent.tagName) || safari && !safari2 )
-					border( offsetParent );
-					
-				// Add the document scroll offsets if position is fixed on any offsetParent
-				if ( !fixed && offsetParent.css("position") == "fixed" )
-					fixed = true;
-			
-				// Set offsetChild to previous offsetParent unless it is the body element
-				offsetChild  = /^body$/i.test(offsetParent.tagName) ? offsetChild : offsetParent;
-				// Get next offsetParent
-				offsetParent = offsetParent.offsetParent;
-			}
-		
-			// Get parent scroll offsets
-			while ( parent && parent.tagName && !/^body|html$/i.test(parent.tagName) ) {
-				// Remove parent scroll UNLESS that parent is inline or a table to work around Opera inline/table scrollLeft/Top bug
-				if ( !/^inline|table.*$/i.test(parent.css("display")) )
-					// Subtract parent scroll offsets
-					add( -parent.scrollLeft, -parent.scrollTop );
-			
-				// Mozilla does not add the border for a parent that has overflow != visible
-				if ( mozilla && parent.css("overflow") != "visible" )
-					border( parent );
-			
-				// Get next parent
-				parent = parent.parentNode;
-			}
-		
-			// Safari <= 2 doubles body offsets with a fixed position element/offsetParent or absolutely positioned offsetChild
-			// Mozilla doubles body offsets with a non-absolutely positioned offsetChild
-			if ( (safari2 && (fixed || offsetChild.css("position") == "absolute")) || 
-				(mozilla && offsetChild.css("position") != "absolute") )
-					add( -doc.body.offsetLeft, -doc.body.offsetTop );
-			
-			// Add the document scroll offsets if position is fixed
-			if ( fixed )
-				add(Math.max(doc.documentElement.scrollLeft, doc.body.scrollLeft),
-					Math.max(doc.documentElement.scrollTop,  doc.body.scrollTop));
-		}
-
-		// Return an object with top and left properties
-		results = { top: top, left: left };
-	}
-
-	function border(elem) {
-		add( elem.curCSS("borderLeftWidth", true), elem.curCSS("borderTopWidth", true) );
-	}
-
-	function add(l, t) {
-		left += parseInt(l) || 0;
-		top += parseInt(t) || 0;
-	}
-
-	return results;
+function storm_getcoords(element) {
+  var coords = {x: 0, y: 0};
+  while (element) {
+    coords.x += element.offsetLeft;
+    coords.y += element.offsetTop;
+    element = element.offsetParent;
+  }
+  return coords;
 };
+
